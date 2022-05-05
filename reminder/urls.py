a47f7e0c460332aae5322a8e9622aeea049a94e6
Aline <aline.ammann@gmx.ch>
2022-05-05 13:34:16 +0200

    changed redirect for settings and replaced id in url for uuid

--- reminder/urls.py
+++ reminder/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('certificate/upload', views.upload_certificate, name='upload_certificate'),
-    path('certificate/<int:certId>', views.show_certificate, name='show_certificate'),
+    path('certificate/<uuid:certUid>', views.show_certificate, name='show_certificate'),
     path('certificate/<uuid:uid>/settings', views.settings, name='settings'),
     path('password_reset', views.password_reset_request, name="password_reset"),    #ACCOUNTS APP?
     path('password_set/<int:userId>', views.password_set_request, name="password_set"), #ACCOUNTS APP?
