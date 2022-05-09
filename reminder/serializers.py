d59cb2873cb9ec0a9f854c675f8f56e468051675
Aline <aline.ammann@gmx.ch>
2022-05-09 10:38:19 +0200

    user password not shown in GET

--- reminder/serializers.py
+++ reminder/serializers.py
@@ -9,7 +9,10 @@ class CertificateSerializer(serializers.HyperlinkedModelSerializer):
 class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('__all__')
+        fields = ('url', 'username', 'email', 'is_active', 'is_staff', 'is_superuser',
+                'last_login', 'date_joined', 'password',)
+        read_only_fields = ('is_active', 'last_login', 'date_joined', 'is_staff', 'is_superuser')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
 class ReminderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
